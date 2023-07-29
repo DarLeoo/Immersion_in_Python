5040479d9af4a9a3f63c3e1fffb38e1beed142c7
Lev Sobolenkov <dangerrrleo@gmail.com>
2023-07-29 03:02:11 +0300

    Добавлена домашняя работа

--- Урок 2. Простые типы данных/Домашняя работа/task_2.py
+++ Урок 2. Простые типы данных/Домашняя работа/task_2.py
@@ -2,3 +2,20 @@
   Напишите программу, которая получает целое число и возвращает его шестнадцатеричное строковое представление.
   Функцию hex используйте для проверки своего результата.
 """
+
+
+num = 88
+
+hex_dict = {10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
+hex_string = ''
+
+print(hex(num))
+while num > 0:
+    remainder = num % 16
+    if remainder >= 10:
+        hex_string = hex_dict[remainder] + hex_string
+    else:
+        hex_string = str(remainder) + hex_string
+    num //= 16
+
+print(f"Шестнадцатеричное представление числа: {hex_string}")
--- Урок 2. Простые типы данных/Домашняя работа/task_3.py
+++ Урок 2. Простые типы данных/Домашняя работа/task_3.py
@@ -2,3 +2,38 @@
 Напишите программу, которая принимает две строки вида “a/b” - дробь с числителем и знаменателем.
 Программа должна возвращать сумму и произведение* дробей. Для проверки своего кода используйте модуль fractions.
 """
+from fractions import Fraction
+
+
+def simplify_fraction(numerator, denominator):
+    a, b = numerator, denominator
+    while b:
+        a, b = b, a % b
+    return numerator // a, denominator // a
+
+
+def add_fractions(fraction1, fraction2):
+    num1, den1 = map(int, fraction1.split('/'))
+    num2, den2 = map(int, fraction2.split('/'))
+    numerator = num1 * den2 + num2 * den1
+    denominator = den1 * den2
+    return simplify_fraction(numerator, denominator)
+
+
+def multiply_fractions(fraction1, fraction2):
+    num1, den1 = map(int, fraction1.split('/'))
+    num2, den2 = map(int, fraction2.split('/'))
+    numerator = num1 * num2
+    denominator = den1 * den2
+    return simplify_fraction(numerator, denominator)
+
+
+fraction1_str = input("Введите первую дробь в формате a/b: ")
+fraction2_str = input("Введите вторую дробь в формате a/b: ")
+
+
+sum_result = add_fractions(fraction1_str, fraction2_str)
+product_result = multiply_fractions(fraction1_str, fraction2_str)
+print(f"Сумма дробей: {sum_result[0]}/{sum_result[1]}")
+print(f"Произведение дробей: {product_result[0]}/{product_result[1]}")
+
