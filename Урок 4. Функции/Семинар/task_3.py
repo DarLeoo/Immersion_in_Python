b6945296a3c773e3803a82739ee2d211f1fd77f4
Lev Sobolenkov <dangerrrleo@gmail.com>
2023-08-02 01:30:47 +0300

    Добавлены решения для 3,4 задания семинара

--- Урок 4. Функции/Семинар/task_3.py
+++ Урок 4. Функции/Семинар/task_3.py
@@ -5,3 +5,21 @@
 ✔ Диапазон пар ключ-значение от наименьшего из введённых
 пользователем чисел до наибольшего включительно.
 """
+
+my_str = "1 4"
+
+
+def create_dict(text: str) -> dict[str, int]:
+    result = {}
+    start, end = sorted(map(int, text.split()))
+    for i in range(start, end + 1):
+        result[chr(i)] = i
+    return result
+
+
+print(create_dict(my_str))
+
+
+
+
+
--- Урок 4. Функции/Семинар/task_4.py
+++ Урок 4. Функции/Семинар/task_4.py
@@ -5,3 +5,17 @@
 ✔ Как вариант напишите сортировку пузырьком.
 Её описание есть в википедии.
 """
+
+
+def sort_num(num_list: list):
+    for i in range(len(num_list)):
+        for j in range(i, len(num_list)):
+            if num_list[i] > num_list[j]:
+                num_list[i], num_list[j] = num_list[j], num_list[i]
+    return num_list
+
+
+num_list = [1, 3, 4, 11, 10, 9]
+
+
+print(sort_num(num_list))
