f972e9238628d9fbf2c2eb6a2f483b49a3d9d217
Lev Sobolenkov <dangerrrleo@gmail.com>
2023-08-10 00:04:56 +0300

    Решено домашнее задание

--- Урок 5. Интераторы и генераторы/Домашняя работа/task_1.py
+++ Урок 5. Интераторы и генераторы/Домашняя работа/task_1.py
@@ -2,3 +2,11 @@
 Напишите функцию, которая принимает на вход строку — абсолютный путь до файла.
 Функция возвращает кортеж из трёх элементов: путь, имя файла, расширение файла.
 """
+
+
+def split_path_info(path: str):
+    *absolut, file = path.split('/')
+    return '/'.join(absolut), *file.split('.')
+
+
+print(split_path_info("C:/Users/Lev/Desktop/test.txt"))
