6a3e0b579c030f4f7073fddb1a6e01e79115a994
DarLeo <dangerrrleo@gmail.com>
2023-08-13 12:12:59 +0300

    Добавлены решения для 1 и 2 задания в семинаре

---
+++ Урок 6/Семинар/task_2.py
@@ -0,0 +1,29 @@
+"""
+Улучшаем задачу 1.
+Добавьте возможность запуска функции “угадайки”
+из модуля в командной строке терминала.
+Строка должна принимать от 1 до 3 аргументов: параметры вызова функции.
+Для преобразования строковых аргументов командной строки
+в числовые параметры используйте генераторное выражение.
+"""
+import random as rnd
+from sys import argv
+
+
+def game(start: int=1, end: int=4, tries: int=3) -> bool:
+    random_num = rnd.randint(start, end)
+    for _ in range(tries):
+        num_from_user = (int(input("Отгадайте число ")))
+        if random_num > num_from_user:
+            print("Введенное Вами число меньше загаданного")
+        elif random_num < num_from_user:
+            print("Введенное Вами число больше загаданного")
+        else:
+            break
+    return random_num == num_from_user
+
+
+if __name__ == '__main__':
+    arguments = map(int, argv[1:])
+    print(game(*arguments))
+
