c0458e228019c9924dad1bc8449bb7084759ff94
lev.sobolenkov <dangerrrleo@gmail.com>
2023-08-16 10:48:57 +0300

    Тест

---
+++ Урок 7.Файлы и файловая система/Семинар/task_1.py
@@ -0,0 +1,20 @@
+"""
+✔ Напишите функцию, которая заполняет файл
+(добавляет в конец) случайными парами чисел.
+✔ Первое число int, второе - float разделены вертикальной чертой.
+✔ Минимальное число - -1000, максимальное - +1000.
+✔ Количество строк и имя файла передаются как аргументы функции
+"""
+from random import randint, uniform
+
+
+def create_file(name_file: str, count_line: int) -> None:
+    name_file += '.txt'
+    with open(name_file, 'a+', encoding='utf=8') as f:
+        for _ in range(count_line + 1):
+            f.write(f'{randint(-1000, 1000)} | {uniform(-1000, 1000)}\n')
+
+
+if __name__ == '__main__':
+    create_file(name_file="testfile",
+                count_line=5)
