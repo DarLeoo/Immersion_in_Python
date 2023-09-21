8a17b7463bc533c81ed0afd9b2f497f1f3151d6f
lev.sobolenkov <dangerrrleo@gmail.com>
2023-09-21 19:21:42 +0300

    Добавлен 13 семинар и дз

---
+++ Урок 13. Исключения/Домашняя_работа/task_1_1.py
@@ -0,0 +1,35 @@
+"""
+Создайте класс окружность.
+Класс должен принимать радиус окружности при создании
+экземпляра.
+У класса должно быть два метода, возвращающие длину
+окружности и её площадь.
+"""
+from task_1 import RadiusError
+import math
+
+
+class Circle:
+    _pi = math.pi
+
+    def __init__(self, radius) -> None:
+        self.validate_radius(radius)
+        self.radius = radius
+
+    def calc_len(self):
+        return self._pi * self.radius * 2
+
+    def validate_radius(self, radius):
+        if radius < 0:
+            raise RadiusError
+
+    def calc_area(self):
+        return self._pi * self.radius ** 2
+
+
+if __name__ == '__main__':
+    c1 = Circle(-1)
+
+
+
+
---
+++ Урок 13. Исключения/Семинар/task_3.py
@@ -0,0 +1,17 @@
+"""
+Создайте класс с базовым исключением и дочерние классы исключения:\
+○ ошибка уровня,\
+○ ошибка доступа.
+"""
+
+
+class MyException(Exception):
+    pass
+
+
+class MyExceptionLevel(MyException):
+    pass
+
+
+class MyExceptionAccess(MyException):
+    pass
